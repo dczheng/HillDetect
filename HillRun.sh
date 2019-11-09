d4838bc9fd6b4b52b7e29ca6a9638f53a41e1816
dczheng <dczheng21@outlook.com>
2019-11-09 20:00:22 +0800

    update

--- HillRun.sh
+++ HillRun.sh
@@ -5,7 +5,14 @@ make
 
 ./HillDetect hill.in
 ./plot.py hill.in 15 
-#./gen_csv_catalog.py hill.in
+#./gen_catalog.py hill.in
 
 ./HillDetect hill_mask.in
 ./plot.py hill_mask.in 15 
+#./gen_catalog.py hill_mask.in
+
+add_hills.py hill.in 
+
+./HillDetect hill_add.in
+./plot.py hill_add.in fits/test.fits
+#./gen_catalog.py hill_add.in
